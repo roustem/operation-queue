a3a90f615ec5b29c47e9af5651d4b26ba8866e98
ab-cgh <>
2020-08-06 12:17:21 -0500

    Basic result passbacks

--- src/database.rs
+++ src/database.rs
@@ -1,75 +1,117 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use rusqlite::{Connection, params};
-use std::sync::mpsc::{self, Sender, Receiver};
+use rusqlite::{params, Connection};
+use std::sync::mpsc::{self, Receiver, Sender};
 
+#[derive(Debug)]
 pub enum Error {
-
+    Save,
+    GetValue,
 }
 
-pub type OperationResult = std::result::Result<(), Error>;
-pub type DatabaseOperation = Box<dyn Fn(Transaction) -> OperationResult + Send>;
+#[derive(Debug)]
+pub enum ResponseType {
+    String(String),
+}
+
+pub type OperationResult<T> = std::result::Result<T, Error>;
+pub type DatabaseOperation = Box<dyn Fn(Transaction) -> OperationResult<ResponseType> + Send>;
 
 pub struct Transaction<'conn> {
     conn: &'conn Connection,
 }
 
 impl<'conn> Transaction<'conn> {
-    pub fn get_value(&self, id: i64) -> rusqlite::Result<String> {
-        let r: String = self.conn.query_row("SELECT value FROM config WHERE id = ?1", params![id], |row| {
-            row.get(0)
-        })?;
-        Ok(r)
+    pub fn get_value(&self, id: i64) -> OperationResult<ResponseType> {
+        let out = self
+            .conn
+            .query_row(
+                "SELECT value FROM config WHERE id = ?1",
+                params![id],
+                |row| row.get(0),
+            )
+            .map_err(|_| Error::GetValue)?;
+
+        Ok(ResponseType::String(out))
     }
 
-    pub fn save_value(&self, id: i64, value: &str) -> rusqlite::Result<usize> {
-        self.conn.execute("INSERT INTO config (id, value) VALUES (?1, ?2)", params![id, value])
+    pub fn save_value(&self, id: i64, value: &str) -> OperationResult<()> {
+        self.conn
+            .execute(
+                "INSERT INTO config (id, value) VALUES (?1, ?2)",
+                params![id, value],
+            )
+            .map_err(|_| Error::Save)
+            .map(|_| ())
     }
 }
 
+struct Operation {
+    name: &'static str,
+    block: DatabaseOperation,
+    result_passback: Sender<OperationResult<ResponseType>>,
+}
+
+#[derive(Debug)]
 pub struct Db {
-    sender: std::sync::mpsc::Sender<DatabaseOperation>,
+    sender: std::sync::mpsc::Sender<Operation>,
     tx_queue: std::thread::JoinHandle<()>,
 }
 
 impl Db {
     pub fn new() -> Self {
-        let (sender, receiver) = mpsc::channel::<DatabaseOperation>();
-        let thread_builder = std::thread::Builder::new().name("tx-queue".into());
-        let tx_queue = thread_builder.spawn(move || {
-            let conn = Connection::open_in_memory().unwrap();
-            Self::create_schema(&conn);
+        let (sender, receiver) =
+            mpsc::channel::<Operation>();
 
-            while let Ok(op) = receiver.recv() {
-                let _ = conn.execute_batch("BEGIN TRANSACTION");
-                let tx = Transaction{conn: &conn};
-                match op(tx) {
-                    Ok(_) => {
-                        let _ = conn.execute_batch("COMMIT");
-                    }
-                    Err(_) => {
-                        let _ = conn.execute_batch("ROLLBACK");
+        let thread_builder = std::thread::Builder::new().name("tx-queue".into());
+        let tx_queue = thread_builder
+            .spawn(move || {
+                let conn = Connection::open_in_memory().unwrap();
+                Self::create_schema(&conn);
+
+                while let Ok(op) = receiver.recv() {
+                    let _ = conn.execute_batch("BEGIN TRANSACTION");
+                    let tx = Transaction { conn: &conn };
+                    println!("Running transaction '{}' on thread {}", op.name, std::thread::current().name().unwrap());
+                    match (op.block)(tx) {
+                        Ok(resp) => {
+                            let _ = conn.execute_batch("COMMIT");
+                            op.result_passback.send(Ok(resp)).unwrap();
+
+                        }
+                        Err(e) => {
+                            let _ = conn.execute_batch("ROLLBACK");
+                            println!("Transaction {} failed, rolling back!", op.name);
+                            op.result_passback.send(Err(e)).unwrap();
+                        }
                     }
                 }
-            }
-            let _ = conn.close();
-        }).unwrap();
+                let _ = conn.close();
+            })
+            .unwrap();
 
-        Self {
-            sender,
-            tx_queue,
-        }
+        Self { sender, tx_queue }
     }
 
-    pub fn create_schema(conn: &Connection) {
-        if let Err(e) = conn.execute_batch("CREATE TABLE IF NOT EXISTS config (id INTEGER PRIMARY KEY, value TEXT)") {
+    fn create_schema(conn: &Connection) {
+        if let Err(e) = conn
+            .execute_batch("CREATE TABLE IF NOT EXISTS config (id INTEGER PRIMARY KEY, value TEXT)")
+        {
             println!("ERROR: {}", e);
         }
     }
 
-    pub fn perform(&self, op: DatabaseOperation) {
+    pub fn transaction(&self, name: &'static str, op: DatabaseOperation) -> OperationResult<ResponseType> {
+        let (passback, rx) = std::sync::mpsc::channel();
+        let op = Operation {
+            block: op,
+            name,
+            result_passback: passback,
+        };
+
         self.sender.send(op).unwrap();
+        rx.recv().unwrap()
     }
 
     pub fn wait_to_complete(self) {
--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,21 @@ fn main() {
     println!("thread: {:?}", thread::current().name());
 
     let db = Db::new();
-    db.perform(Box::new(|tx| {
-        tx.save_value(1, "value 1");
-        println!("transaction #1 ({}) {:?}", tx.get_value(1).unwrap(), thread::current().name());
-        Ok(())
+
+    let value_in = "Some value #1".to_string();
+    let res = db.transaction("first thing", Box::new(move |tx| {
+        tx.save_value(1, &value_in).unwrap();
+        tx.get_value(1)
     }));
 
+    println!("transaction #1 ({:?}) result on thread '{:?}'", res.unwrap(), thread::current().name());
 
-    db.perform(Box::new(|tx| {
-        tx.save_value(2, "value too awesome");
-        println!("transaction #2 ({}) {:?}", tx.get_value(2).unwrap(), thread::current().name());
-        Ok(())
+    let res_2 = db.transaction("second thing", Box::new(|tx| {
+        tx.save_value(2, "value too awesome").unwrap();
+        tx.get_value(2)
     }));
 
+    println!("transaction #2 ({:?}) result on thread '{:?}'", res_2.unwrap(), thread::current().name());
 
     db.wait_to_complete();
 }
